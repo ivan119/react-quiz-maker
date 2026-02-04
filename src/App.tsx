9fc826a539822f6cf797f8d6b567c8eb25e1a85b
Ivan Kelava <ivankelava991@gmail.com>
2026-02-04 14:08:54 +0100

    chore: remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import Test from './components/Test';
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-
   return (
-    <>
-      <h1>Hello World</h1>
-
-      <Test
-        title="Strict typing check"
-        count={count}
-        status="loading"
-        onIncrement={(value) => setCount(value)}
-      />
-    </>
+    <main className="app-container">
+      <section className="hero">
+        <h1 className="gradient-text">Rejd - Quiz Maker</h1>
+        <p className="subtitle">Create, share, and solve quiz with style.</p>
+        <div className="cta-group">
+          <button className="btn-primary">Get Started</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
