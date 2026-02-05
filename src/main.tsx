9e2fb4202376822d006c6aa7396c871a3029579e
Ivan Kelava <ivankelava991@gmail.com>
2026-02-05 10:32:34 +0100

    chore: enable mocking based on env file

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,27 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './app/App';
 import theme from './app/theme';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+async function enableMocking() {
+  const isDevelopment = import.meta.env.MODE === 'development';
+  const isMockEnabled = import.meta.env.VITE_ENABLE_MOCKS === 'true';
+  if (!isDevelopment || !isMockEnabled) {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+});
