58e0121c9a3c27341d8e2c52420e95c1b93d7caa
Ivan Kelava <ivankelava991@gmail.com>
2026-02-06 16:16:03 +0100

    chore: add max validation

--- src/app/lib/validators/quiz.schema.ts
+++ src/app/lib/validators/quiz.schema.ts
@@ -1,15 +1,17 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const quizSchema = z.object({
-    name: z.string().min(3, "Name must be at least 3 characters"),
-    questions: z
-        .array(
-            z.object({
-                question: z.string().min(1, "Question text is required"),
-                answer: z.string().min(1, "Answer is required"),
-            })
-        )
-        .min(15, "At least 15 questions is required to save quiz"),
+  // TODO: add min of 15 questions after development
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  questions: z
+    .array(
+      z.object({
+        question: z.string().min(1, 'Question text is required'),
+        answer: z.string().min(1, 'Answer is required'),
+      })
+    )
+    .min(2, 'At least 2 questions is required to save quiz')
+    .max(50, 'Quiz can have maximum 50 questions'),
 });
 
 export type QuizFormValues = z.infer<typeof quizSchema>;
