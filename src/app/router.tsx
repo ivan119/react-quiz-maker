a9fbe9da387b29eedfff4b6b61e9c66038a6d7ae
Ivan Kelava <ivankelava991@gmail.com>
2026-02-08 19:04:32 +0100

    chore: router configuration

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,18 +1,38 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import MainLayout from './components/Layout/Main';
 import QuizCreatePage from './routes/QuizCreatePage';
 import QuizEditPage from './routes/QuizEditPage';
 import QuizListPage from './routes/QuizListPage';
-const QuizSolvePage = () => <div>Solve quiz</div>;
+import LoginPage from './routes/LoginPage';
+import QuizSolvePage from './routes/QuizSolvePage';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
 
 export const router = createBrowserRouter([
   {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
     element: <MainLayout />,
     children: [
-      { path: '/', element: <QuizListPage /> },
-      { path: '/quiz/create', element: <QuizCreatePage /> },
-      { path: '/quiz/:id/edit', element: <QuizEditPage /> },
-      { path: '/quiz/:id/solve', element: <QuizSolvePage /> },
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { path: '/', element: <QuizListPage /> },
+          { path: '/quiz/:id/solve', element: <QuizSolvePage /> },
+          {
+            element: <ProtectedRoute adminOnly />,
+            children: [
+              { path: '/quiz/create', element: <QuizCreatePage /> },
+              { path: '/quiz/:id/edit', element: <QuizEditPage /> },
+            ],
+          },
+        ],
+      },
     ],
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
   },
 ]);
