3bece0b252f00f149fb3d7675e1beb7a9e341570
Ivan Kelava <ivankelava991@gmail.com>
2026-02-08 19:01:18 +0100

    chore: wrap app with AuthProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { NotificationProvider } from './context/NotificationContext';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
-    <NotificationProvider>
-      <RouterProvider router={router} />
-    </NotificationProvider>
+    <AuthProvider>
+      <NotificationProvider>
+        <RouterProvider router={router} />
+      </NotificationProvider>
+    </AuthProvider>
   );
 }
